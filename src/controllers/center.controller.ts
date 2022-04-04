b6e1c1ac7473224fceec3f8ca70233f4160edbd2
Enzo Bomark <71819938+EnzoBomark@users.noreply.github.com>
2022-04-04 12:37:01 +0200

    feat: add center controller and routes

---
+++ src/controllers/center.controller.ts
@@ -0,0 +1,136 @@
+import { Center, City } from '../models';
+import { pick, clean, pagination } from '../utils';
+import { throwError } from '../middleware';
+import { findOrFail } from '../services';
+import database from '../database';
+
+const getCenter = async (req: Req<param>, res: Res<Center>) => {
+  const { id } = req.params;
+
+  try {
+    const center = await findOrFail(
+      Center.findOne({
+        where: { id },
+      })
+    );
+
+    return res.status(200).send(center);
+  } catch (err) {
+    return throwError('Cannot get center', err);
+  }
+};
+
+const getCenters = async (
+  req: Req<query<{ centerIds: Ids; cityIds: Ids }>>,
+  res: Res<ReadonlyArray<Center>>
+) => {
+  const { query } = req;
+
+  try {
+    const centers = await Center.findAll(
+      pagination(
+        {
+          where: clean({ id: query.centerIds }),
+          include: [
+            {
+              model: City,
+              as: 'city',
+              where: clean({ id: query.cityIds }),
+            },
+          ],
+        },
+        { page: query.page, pageSize: query.pageSize }
+      )
+    );
+
+    return res.status(200).send(centers);
+  } catch (err) {
+    return throwError('Cannot get centers', err);
+  }
+};
+
+const postCenter = async (req: Req<body<Center>>, res: Res<Center>) => {
+  const { body } = req;
+
+  try {
+    const center = await database.transaction(async (transaction) => {
+      const T = await Center.create(
+        pick(body, 'name', 'picture', 'address', 'contactUrl', 'bookingUrl'),
+        { transaction }
+      );
+
+      const city = await findOrFail(
+        City.findOne({ where: { id: body.cityId } })
+      );
+
+      await T.setCity(city, { transaction });
+
+      return T;
+    });
+
+    return res.status(201).send(center);
+  } catch (err) {
+    return throwError('Cannot create center', err);
+  }
+};
+
+const putCenter = async (
+  req: Req<body<Partial<Center>>, param>,
+  res: Res<Center>
+) => {
+  const { params, body } = req;
+
+  try {
+    const center = await findOrFail(
+      Center.findOne({
+        where: { id: params.id },
+      })
+    );
+
+    if (body.cityId) {
+      const city = await findOrFail(
+        City.findOne({
+          where: { id: body.cityId },
+        })
+      );
+
+      center.setCity(city);
+    }
+
+    await center.update(
+      pick(body, 'name', 'picture', 'address', 'contactUrl', 'bookingUrl')
+    );
+
+    return res.status(200).send(center);
+  } catch (err) {
+    return throwError('Cannot update center', err);
+  }
+};
+
+const deleteCenter = async (req: Req<param>, res: Res<string>) => {
+  const { params } = req;
+
+  try {
+    const center = await findOrFail(
+      Center.findOne({
+        where: { id: params.id },
+      })
+    );
+
+    await center.destroy();
+
+    return res.status(200).send('Center has been deleted');
+  } catch (err) {
+    return throwError('Cannot delete center', err);
+  }
+};
+
+const center = {
+  getCenter,
+  getCenters,
+  postCenter,
+  putCenter,
+  deleteCenter,
+};
+
+export default center;
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,13 @@
 import country from './country.controller';
 import city from './city.controller';
 import profile from './profile.controller';
+import center from './center.controller';
 
 const controllers = {
   ...country,
   ...city,
   ...profile,
+  ...center,
 };
 
 export default controllers;
